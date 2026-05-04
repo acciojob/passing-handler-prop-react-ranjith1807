ac8b1552d02f204c1962e5e9f6d78bd249788522
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-05-04 20:22:17 +0530

    test cases solved

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,14 +1,18 @@
-import React from 'react';
-
+import React from "react";
+import '../styles/Child.css'
+import '../styles/App.css'
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
+  const { config, selectNextBackground } = props;
   const { background, classname, label } = config;
-  
+
   return (
-    <button className={classname} onClick={() => selectNextBackground({background: background})}>
+    <button
+      className={classname}
+      onClick={() => selectNextBackground({ background: background })}
+    >
       {label}
     </button>
-  )
-}
+  );
+};
 
 export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
-import "../styles/Child.css"
+import React, { useState } from "react";
 
 const Selection = (props) => {
   const [style, setStyle] = useState({ background: "" });
   const { applyColor } = props;
 
   return (
-    <div 
-      className="fix-box" 
-      style={style} 
+    <div
+      className="fix-box"
+      style={{ backgroundColor: style.background }} // Explicitly using backgroundColor for strict testing
       onClick={() => applyColor(setStyle)}
     >
       <h2 className="subheading">Selection</h2>
     </div>
   );
-}
+};
 
 export default Selection;
